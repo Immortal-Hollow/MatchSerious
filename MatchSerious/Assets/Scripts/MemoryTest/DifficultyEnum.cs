4224b86908fa6147e72c528fa1daa6866078abe4
billidev200 <vasilislampou2005@gmail.com>
2025-02-12 22:43:51 +0200

    Created DifficultyEnum script

---
+++ MatchSerious/Assets/Scripts/MemoryTest/DifficultyEnum.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enum representing different difficulty levels
+public enum DifficultyEnum
+{
+    Easy,    // Easy difficulty, likely for beginners or less challenging gameplay
+    Normal,  // Normal difficulty, a balanced level for most players
+    Hard     // Hard difficulty, for experienced players or more challenging gameplay
+}
