0a5233a7cfb1146941da2b1cd5c1f3f1057e931d
billidev200 <vasilislampou2005@gmail.com>
2025-02-17 01:07:14 +0200

    App Build Compiling error Fix

--- MatchSerious/Assets/Scripts/MemoryTest/TweenerUI.cs
+++ MatchSerious/Assets/Scripts/MemoryTest/TweenerUI.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using static UnityEditor.MaterialProperty;
 
 // Enum defining possible animation types
 public enum UIAnimationType
